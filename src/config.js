e510293c82d8410ea0ab12b39348b34df7dbf4ca
midimurphdesigns <midimurphdesigns@gmail.com>
2018-07-12 11:17:20 -0700

    redeploy to netlify

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
 
-//if you go back to redeploy your app, use this command 
+//if you go back to redeploy your app, use this command
 // REACT_APP_API_BASE_URL="https://musixchange.herokuapp.com/api" npm run build
